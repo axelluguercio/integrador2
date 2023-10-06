c7a476a12a39278a870349dcb2c366d3ff19982e
Franco Guerra <francolguerra@gmail.com>
2023-10-06 20:16:40 -0300

    Se agraga clase EstudianteCarrera, se agrega clase al persistence

--- src/main/java/tp2/entidades/EstudianteCarrera.java
+++ src/main/java/tp2/entidades/EstudianteCarrera.java
@@ -1,29 +1,35 @@
 package tp2.entidades;
-import tp2.dto.DTOEstudiante;
-import tp2.entidades.Carrera;
-import tp2.entidades.Estudiante;
+
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
+@Table(name = "estudiante-carrera")
+
 public class EstudianteCarrera {
 
 
     @Id
     @Column(name = "id_estudiante_carrera")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
+
     @Column(name = "carrera")
-    Carrera carrera;
+    private Carrera carrera;
+
     @Column(name = "anio")
-    int anio;
+    private int anio;
+
     @Column(name = "duracion")
-    int duracion;
-    @Column(name = "estudiantes_inscriptos")
-    List<Estudiante> estudiantesInscriptos;
-    @Column(name = "estudiantes_egresados")
-    List<Estudiante> estudiantesEgresados;
+    private int duracion;
 
+    private Estudiante estudiante;
 
+    public EstudianteCarrera(Carrera carrera, int anio, int duracion, Estudiante estudiante) {
+
+        this.carrera = carrera;
+        this.anio = anio;
+        this.duracion = duracion;
+        this.estudiante = estudiante;
+    }
 
 }
